a424781aa58bad26eae5cebea47ebeffa10664f5
Julish <juxishimwe@gmail.com>
2023-09-26 16:56:24 +0200

    start the project

--- solid-stairltd/tailwind.config.js
+++ solid-stairltd/tailwind.config.js
@@ -7,22 +7,29 @@ export default {
   theme: {
     extend: {
       colors: {
-        "gray-20": "F9F9F9", /*landing background color*/
-        "gray-50": "50663F", /*dark-light-green color of web*/
-        "gray-100": "D6D7D9", /*about-rightside bg-color*/
-        "gray-500": "EBEBEB" /*about-mainbgcolor*/
+        "gray-20": "#F9F9F9", /*landing background color*/
+        "gray-50": "#50663F", /*dark-light-green color of web*/
+        "gray-70": "#0000FF", 
+        "gray-100": "#D6D7D9", /*about-rightside bg-color*/
+        "gray-500": "#EBEBEB", /*about-mainbgcolor*/
+        "gray-700": "#5E0000",
+        "primary-100": "#FFE1E0",
+        "primary-300": "#FFA6A3",
+        "primary-500": "#FF6B66",
+        "secondary-400": "#FFCD5B",
+        "secondary-500": "#FFC132",
       }, 
       backgroundImage: (theme) => ({
         "gradient-yellowred":
           "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
-        "mobile-home": "url('./assets/HomePageGraphic.png')",
+        "mobile-home": "url('./assets/HomePage.jpg')",
       }),
       fontFamily: {
         dmsans: ["DM Sans", "sans-serif"],
         montserrat: ["Montserrat", "sans-serif"],
       },
       content: {
-        evolvetext: "url('./assets/EvolveText.png')",
+        evolvetext: "url('./assets/EvolveText.png ')",
         abstractwaves: "url('./assets/AbstractWaves.png')",
         sparkles: "url('./assets/Sparkles.png')",
         circles: "url('./assets/Circles.png')",
