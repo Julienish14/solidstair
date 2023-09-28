a25be23926a0d7f15768e0dbe468974d63398c99
Julien Ishimwe <julien.ishimwe@tek-experts.com>
2023-09-29 00:51:09 +0200

    update on ch-build-proj

--- solid-stairltd/src/App.tsx
+++ solid-stairltd/src/App.tsx
@@ -1,8 +1,10 @@
 import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
 
 function App() {
   return <div className="app bg-gray-20">
     <Navbar />
+    <Home />
   </div>;
 }
   
