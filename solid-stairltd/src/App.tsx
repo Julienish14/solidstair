2d84a3b020490933cfab833b4feebd7b79a76e86
Julish <juxishimwe@gmail.com>
2023-10-02 17:12:56 +0200

    make it more functional

--- solid-stairltd/src/App.tsx
+++ solid-stairltd/src/App.tsx
@@ -1,11 +1,14 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import { useState } from "react";
+import { SelectedPage } from "./shared/types";
 
 
 function App() {
 
-  const [selectedPage, setSelectedPage] = useState("home"); 
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+    ); 
   return <div className="app bg-gray-20">
     <Navbar 
        selectedPage={selectedPage}
